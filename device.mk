c5ac4264b2579c7d7a817e5392ca84ab327302fa
Carlo Dee <carlodee.official@proton.me>
2023-04-04 21:01:00 +0800

    Test: Add wifi packages.

--- device.mk
+++ device.mk
@@ -432,6 +432,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     android.hardware.wifi.hostapd@1.0.vendor \
+    hostapd \
+    libwifi-hal-qcom \
+    libwpa_client \
+    wpa_supplicant \
+    wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
