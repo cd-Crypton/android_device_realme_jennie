6b3659f63a3e9e0fad7d750334d02dd4611593cc
Carlo Dee <carlodee.official@proton.me>
2023-04-05 17:32:52 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,10 +79,11 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/device/device_framework_matrix.xml \
-    $(DRVICE_PATH)/configs/device/vendor_framework_compatibility_matrix.xml \
-DEVICE_FRAMEWORK_MANIFEST_FILE += 
+    $(DEVICE_PATH)/configs/device/vendor_framework_compatibility_matrix.xml \
+    $(DEVICE_PATH)/configs/device/lineage_device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/device/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/device/manifest_taro.xml
+ODM_MANIFEST_FILES := $(DEVICE_PATH)/configs/device/manifest_odm.xml
 
 
 # Kernel
