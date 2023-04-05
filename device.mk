2384daf8d0240ee265923b23d9a5849521f57a10
Carlo Dee <carlodee.official@proton.me>
2023-04-05 11:37:34 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,10 +5,11 @@
 #
 
 LOCAL_PATH := device/realme/jennie
+
 # Prebuilt Kernel Binary
 TARGET_KERNEL_VERSION := 5.10
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilts/kernel:kernel \
+    $(LOCAL_PATH)/prebuilts/Image:kernel \
     $(LOCAL_PATH)/prebuilts/dtb.img:dtb.img
 
 # Enable updating of APEXes
