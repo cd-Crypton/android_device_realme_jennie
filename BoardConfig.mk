b990350666dda9412bc60cee22ca9c76dc501c46
Carlo Dee <carlodee.official@proton.me>
2023-04-06 14:55:27 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,8 +87,20 @@ ODM_MANIFEST_FILES := $(DEVICE_PATH)/configs/device/manifest_odm.xml
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 4
-BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000 msm_geni_serial.con_enabled=0 bootconfig
-BOARD_BOOTCONFIG += androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += \
+     video=vfb:640x400,bpp=32,memsize=3072000 \
+     msm_geni_serial.con_enabled=0 \
+     cgroup.memory=nokmem,nosocket \
+     service_locator.enable=1 \
+     swiotlb=noforce \
+     loop.max_part=7
+BOARD_BOOTCONFIG += \
+     androidboot.hardware=qcom \
+     androidboot.memcg=1
+     androidboot.usbcontroller=a600000.dwc3 \
+     androidboot.bootdevice=1d84000.ufshc \
+     androidboot.force_normal_boot=1 \
+     androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_KERNEL_IMAGE_NAME := Image
@@ -192,11 +204,6 @@ SOONG_CONFIG_OPLUS_LINEAGE_TOUCH_HAL_INCLUDE_DIR := $(DEVICE_PATH)/touch/include
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
-
-BOARD_AVB_BOOT_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
-BOARD_AVB_BOOT_ALGORITHM := SHA256_RSA4096
-BOARD_AVB_BOOT_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_BOOT_ROLLBACK_INDEX_LOCATION := 4
 
 BOARD_AVB_VBMETA_SYSTEM := system system_ext product
 BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
